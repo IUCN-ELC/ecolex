212028b9f64a0e00da3babf606036f9474063475
Catalin Badea <catalin.badea392@gmail.com>
2015-01-30 14:39:19 +0200

    Use template for search and results.

--- search/urls.py
+++ search/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, url
 
 from search import views
+from search.views import SearchView
+from search.views import SearchViewWithResults
 
 urlpatterns = patterns('',
-    url(r'^$', views.search, name="search"),
-    url(r'^result/', views.result, name="result"),
+    url(r'^$', SearchView.as_view(), name="homepage"),
+    url(r'^result/', SearchViewWithResults.as_view(), name="results"),
+    url(r'^p/(?P<slug>\w+)/', views.page, name="page"),
 )
--- search/views.py
+++ search/views.py
@@ -1,24 +1,41 @@
 from django import forms
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from datetime import datetime
 
 import pysolr, json
 
-def search(request):
-    return render(request, 'search.html')
+class SearchView(TemplateView):
+    template_name = "search.html"
 
-def result(request):
-    if 'q' in request.GET:
-        solr = pysolr.Solr('http://10.0.0.98:8983/solr/ecolex', timeout=10)
-        solr.optimize()
-        responses = solr.search('decBody:' + request.GET['q'])
-        
+class SearchViewWithResults(SearchView):
+    template_name = "list_results.html"
+
+    def get_context_data(self, **kwargs):
+        ctx = super(SearchViewWithResults, self).get_context_data(**kwargs)
+        return ctx
+
+    def get(self, request, **kwargs):
+        if 'q' in request.GET:
+            solr = pysolr.Solr('http://10.0.0.98:8983/solr/ecolex', timeout=10)
+            solr.optimize()
+
+            user_query = request.GET['q']
+            solr_query = 'trTitleOfText:' + user_query + ' decBody:' + user_query
+            responses = solr.search(solr_query, rows=15)
+
         results = []
         for hit in responses:
-            results.append(json.dumps(hit))
+            #FIXME(catalinb): temporary
+            if hit.get("decPublishDate"):
+                parse_time = lambda date: datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ")
+                hit["decPublishDate"] = map(parse_time, hit["decPublishDate"])
+                parse_body = lambda body: body[:250] + "..."
+                hit["decBody"] = map(parse_body, hit["decBody"])
+            results.append(hit)
 
         return render(request, 'list_results.html', {'results': results, 'query': request.GET['q']})
-    else:
-        message = "Please enter a search term"
 
-    return HttpResponse(message)
+def page(request, slug):
+    return HttpResponse("slug=" + slug)
