a99cd0eba1e488a8d3b68f41f6834750748da5c2
Catalin Badea <catalin.badea392@gmail.com>
2015-02-12 13:58:01 +0200

    fix display date in treaty/details/participants

--- ecolex/templatetags/tags.py
+++ ecolex/templatetags/tags.py
@@ -1,4 +1,5 @@
 from django import template
+from datetime import datetime
 
 register = template.Library()
 
@@ -15,3 +16,8 @@ def join_by(lst, arg):
     if lst and type(lst) is list:
         return arg.join(lst)
     return lst
+
+
+@register.filter
+def parse_date(d):
+    return datetime.strptime(d, '%Y-%m-%dT%H:%M:%SZ').date()
