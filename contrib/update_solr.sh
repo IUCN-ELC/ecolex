0a2a5a37a6829f762685773f4d1c741feddb54c4
Ana Ion <ana.ion4@gmail.com>
2015-02-13 18:22:59 +0200

    [misc] Remove ZK command from the update script

--- contrib/update_solr.sh
+++ contrib/update_solr.sh
@@ -7,4 +7,3 @@ cd solr-4.10.3/example/
 rm -rf "solr/$1/data/index/*" && rm -rf "solr/$1/data/tlog/*"
 cp ~/ecolex-prototype/configs/schema.xml "solr/$1/conf/schema.xml"
 cp ~/ecolex-prototype/configs/data-config.xml "solr/$1/conf/data-config.xml"
-scripts/cloud-scripts/zkcli.sh -cmd upconfig -zkhost  127.0.0.1:2181  -collection $1 -confname $1 -solrhome solr -confdir "solr/$1/conf/"
