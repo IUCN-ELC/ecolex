19db12292558ce06e3dcc823c0ba705d4a45164d
Alex Eftimie <alex@eftimie.ro>
2015-02-18 13:04:59 +0200

    Refactor the homepage buttons behaviour.

--- ecolex/static/js/main.js
+++ ecolex/static/js/main.js
@@ -1,39 +1,43 @@
 var _form_data = null;
 
 $(document).ready(function () {
-    $('[data-filter]').on('click', function() {
-      target = $(this).data('filter')
-      target = $(target);
-      $(this).toggleClass('active');
+    $('[data-filter]').on('click', function () {
+        target = $(this).data('filter')
+        target = $(target);
+        $(this).toggleClass('active');
 
-      if (target.attr('disabled')) {
-        target.removeAttr('disabled');
-      } else {
-        target.attr('disabled', true);
-      }
+        if (target.attr('disabled')) {
+            target.removeAttr('disabled');
+        } else {
+            target.attr('disabled', true);
+        }
     });
 
-   // reload results when the back button was pressed
-   $(window).on("popstate", function(e) {
-      // history states are tagged in order to ignore popstate events
-      // triggered on page load.
-      if (history.state.tag !== "ecolex") {
-        return;
-      }
-      // $(".search-form").deserialize(location.search.substring(1));
-      $(".search-form").deserialize(history.state.data);
-      submit(history.state.data);
-   });
+    // reload results when the back button was pressed
+    $(window).on("popstate", function (e) {
+        // history states are tagged in order to ignore popstate events
+        // triggered on page load.
+        if (history.state.tag !== "ecolex") {
+            return;
+        }
+        // $(".search-form").deserialize(location.search.substring(1));
+        $(".search-form").deserialize(history.state.data);
+        submit(history.state.data);
+    });
 
 
     // initial form value
     var _initial_form_data = $('.search-form').serialize();
 
     init = 0;
-    function push_and_submit() {
-        var data = $('.search-form').serialize();
-        history.pushState({data : data, tag: 'ecolex'} , '', '?' + data);
-        submit(data);
+    function push_and_submit(ajax) {
+        if (ajax) {
+            var data = $('.search-form').serialize();
+            history.pushState({data: data, tag: 'ecolex'}, '', '?' + data);
+            submit(data);
+        } else {
+            $('.search-form').submit();
+        }
     }
 
     function submit(serialized_form_data) {
@@ -55,12 +59,10 @@ $(document).ready(function () {
                     $('main').unblock();
                 }
             });
-            //$('.search-form').submit();
         } else {
-            console.log('No new data');
+            console.log('No new data, not submitting.');
         }
     }
-
 
     function init_all() {
         // initialize tooltips
@@ -74,46 +76,46 @@ $(document).ready(function () {
 
         // Slider
         $("#slider-years")
-          .slider()
-          .on('slide', function(event) {
-              var min = $('#year-min'),
-                  max = $('#year-max');
+            .slider()
+            .on('slide', function (event) {
+                var min = $('#year-min'),
+                    max = $('#year-max');
 
-              min.val(event.value[0]);
-              max.val(event.value[1]);
-          })
-          .on('slideStop', function(event) {
-            var min = $('#year-min'),
-                max = $('#year-max');
+                min.val(event.value[0]);
+                max.val(event.value[1]);
+            })
+            .on('slideStop', function (event) {
+                var min = $('#year-min'),
+                    max = $('#year-max');
 
-            min.val(event.value[0]);
-            max.val(event.value[1]);
+                min.val(event.value[0]);
+                max.val(event.value[1]);
 
-            var form_id = $(min).data('formid');
-            $(form_id).val($(min).val());
+                var form_id = $(min).data('formid');
+                $(form_id).val($(min).val());
 
-            var form_id = $(max).data('formid');
-            $(form_id).val($(max).val());
+                var form_id = $(max).data('formid');
+                $(form_id).val($(max).val());
 
-            push_and_submit();
-          });
+                push_and_submit(true);
+            });
 
         // Year inputs
-        updateYear = function(e) {
-          minEl = $('#year-min');
-          maxEl = $('#year-max');
+        updateYear = function (e) {
+            minEl = $('#year-min');
+            maxEl = $('#year-max');
 
-          min = $(minEl).val();
-          max = $(maxEl).val();
+            min = $(minEl).val();
+            max = $(maxEl).val();
 
-          if (min == '' || typeof min == 'undefined')
-            min = $(minEl).attr('min');
-          if (max == '' || typeof max == 'undefined')
-            max = $(maxEl).attr('max');
+            if (min == '' || typeof min == 'undefined')
+                min = $(minEl).attr('min');
+            if (max == '' || typeof max == 'undefined')
+                max = $(maxEl).attr('max');
 
-          console.log([min, max]);
+            console.log([min, max]);
 
-          $("#slider-years").slider('setValue', [min, max]);
+            $("#slider-years").slider('setValue', [min, max]);
         }
 
         $('#year-min, #year-max').on('change', updateYear);
@@ -136,9 +138,9 @@ $(document).ready(function () {
 
                 $(formid).val(select.val());
                 // submit now for now
-                push_and_submit();
+                push_and_submit(true);
             },
-            onDropdownShown: function(e) {
+            onDropdownShown: function (e) {
                 search = $(e.target).find('.multiselect-search').focus();
             }
         });
@@ -157,10 +159,10 @@ $(document).ready(function () {
             });
         };
 
-       // FIXME(catalinb): duplicate code. Ship it!
-       $('.filter-type.homepage button').click(function (e) {
+        $('.filter-type button').click(function (e) {
             var current = $('#id_type').val() || [];
             var toggle_value = $(this).data('value');
+            var is_homepage = $(this).parents().hasClass('homepage');
             if (current.indexOf(toggle_value) == -1) {
                 current.push(toggle_value);
             } else {
@@ -168,20 +170,10 @@ $(document).ready(function () {
             }
             $('#id_type').val(current);
             // submit now for now
-            $(".search-form").submit();
-        });
-
-        $('.filter-type.results-page button').click(function (e) {
-            var current = $('#id_type').val() || [];
-            var toggle_value = $(this).data('value');
-            if (current.indexOf(toggle_value) == -1) {
-                current.push(toggle_value);
-            } else {
-                current.splice(current.indexOf(toggle_value), 1);
-            }
-            $('#id_type').val(current);
-            // submit now for now
-            push_and_submit();
+            if (is_homepage)
+                push_and_submit(false);
+            else
+                push_and_submit(true);
         });
 
         console.log("set");
@@ -199,7 +191,7 @@ $(document).ready(function () {
                 });
                 $(form_id).val(current);
                 // submit now for now
-                push_and_submit();
+                push_and_submit(true);
             });
 
         // Year controls
@@ -207,7 +199,7 @@ $(document).ready(function () {
             var form_id = $(this).data('formid');
             $(form_id).val($(this).val());
             // submit le form
-            push_and_submit();
+            push_and_submit(true);
         });
 
         // Sortby controls
@@ -215,7 +207,7 @@ $(document).ready(function () {
             e.preventDefault();
             var value = $(this).data('sortby');
             $('#id_sortby').val(value);
-            push_and_submit();
+            push_and_submit(true);
         });
 
         $('button[type=submit]').click(function (e) {
@@ -237,50 +229,49 @@ $(document).ready(function () {
                 'q': $('#id_q').val(),
                 'type': $('#id_type').val()
             };
-            $('.search-form select, .search-form input').each(function() {
+            $('.search-form select, .search-form input').each(function () {
                 $(this).val('')
             });
             $('#id_q').val(data.q);
             $('#id_type').val(data.type);
 
-            push_and_submit();
+            push_and_submit(true);
         });
 
         // Tag manager
+        var substringMatcher = function (strs) {
+            return function findMatches(q, cb) {
+                var matches, substrRegex;
 
-       var substringMatcher = function(strs) {
-          return function findMatches(q, cb) {
-            var matches, substrRegex;
+                // an array that will be populated with substring matches
+                matches = [];
 
-            // an array that will be populated with substring matches
-            matches = [];
+                // regex used to determine if a string contains the substring `q`
+                substrRegex = new RegExp(q, 'i');
 
-            // regex used to determine if a string contains the substring `q`
-            substrRegex = new RegExp(q, 'i');
+                // iterate through the pool of strings and for any string that
+                // contains the substring `q`, add it to the `matches` array
+                $.each(strs, function (i, str) {
+                    if (substrRegex.test(str.value)) {
+                        // the typeahead jQuery plugin expects suggestions to a
+                        // JavaScript object, refer to typeahead docs for more info
+                        matches.push(str);
+                    }
+                });
 
-            // iterate through the pool of strings and for any string that
-            // contains the substring `q`, add it to the `matches` array
-            $.each(strs, function(i, str) {
-              if (substrRegex.test(str.value)) {
-                // the typeahead jQuery plugin expects suggestions to a
-                // JavaScript object, refer to typeahead docs for more info
-                matches.push(str);
-              }
-            });
-
-            cb(matches);
-          };
+                cb(matches);
+            };
         };
 
-        var tagValidator = function(strs) {
+        var tagValidator = function (strs) {
             return function (tag) {
                 return (strs.indexOf(tag) !== -1);
             }
         }
 
-        $(".tag-select").each(function() {
+        $(".tag-select").each(function () {
             var suggestions = [];
-            var preselected= [];
+            var preselected = [];
             $("#options option", this).each(function (i, opt) {
                 selected = console.log($(opt).attr("selected"));
                 entry = {
@@ -291,7 +282,7 @@ $(document).ready(function () {
                 preselected.push(entry);
             });
 
-            $('.tm-input', this).each(function() {
+            $('.tm-input', this).each(function () {
                 var self = this;
                 $(this).tagsManager({
                     tagsContainer: $('<ul/>', { class: 'tm-taglist' }),
@@ -299,7 +290,7 @@ $(document).ready(function () {
                     prefilled: preselected,
                     // onlyTagList: true,
                     // validator: tagValidator(suggestions),
-                }).on("tm:spliced", function (e) {
+                }).on("tm:spliced",function (e) {
                     var formid = $(this).data('formid');
                     var value = $(this).tagsManager('tags');
                     $(formid).val(value);
@@ -311,7 +302,7 @@ $(document).ready(function () {
                     push_and_submit();
                 });
 
-                $(this).wrap( $('<div/>', { class: 'tm-wrapper' }) );
+                $(this).wrap($('<div/>', { class: 'tm-wrapper' }));
                 var label = $('<label/>', {
                     'class': 'tm-label',
                     'for': $(this).attr('id'),
@@ -320,17 +311,17 @@ $(document).ready(function () {
                 $(this).before(label);
 
                 $(this).typeahead({
-                    hint: false,
-                    highlight: true,
-                    minLength: 0,
-                },
-                {
-                    name: 'states',
-                    displayKey: 'displayValue',
-                    source: substringMatcher(suggestions),
-                });
+                        hint: false,
+                        highlight: true,
+                        minLength: 0,
+                    },
+                    {
+                        name: 'states',
+                        displayKey: 'displayValue',
+                        source: substringMatcher(suggestions),
+                    });
 
-                $(this).on('blur', function() {
+                $(this).on('blur', function () {
                     $(this).val('');
                 });
             });
