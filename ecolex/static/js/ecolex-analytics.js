a629bb25877923755fcc431aee1e8f1013332613
Catalin Badea <catalin.badea392@gmail.com>
2015-02-27 12:09:07 +0200

    Correct count for total search count.

--- ecolex/static/js/ecolex-analytics.js
+++ ecolex/static/js/ecolex-analytics.js
@@ -80,7 +80,7 @@ function count_searches() {
     ga('set', 'metric3', 1);
     
     // any search hit
-    ga('set', 'metric3', 5);
+    ga('set', 'metric5', 1);
     return;
   }
 
@@ -99,7 +99,7 @@ function count_searches() {
     // advanced search hit
     ga('set', 'metric4', 1);
     // any search hit
-    ga('set', 'metric3', 5);
+    ga('set', 'metric5', 1);
     return;
   }
 
