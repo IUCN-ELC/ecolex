3d4f62241c6c0425a0ac0658e556c67c5b0594a5
Taygun <taygun.agiali@eaudeweb.ro>
2016-02-04 18:00:43 +0200

    Legislation text index check by url

--- contrib/legislation.py
+++ contrib/legislation.py
@@ -1,17 +1,18 @@
 import os
 import sys
 
+from django.core.wsgi import get_wsgi_application
+
+from utils import EcolexSolr, get_file_from_url
+
 project_path = "/ecolex/"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecolex.settings")
 sys.path.append(project_path)
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-from utils import EcolexSolr, get_file_from_url
-from ecolex.models import DocumentText
 
 
 def update_legislation_full_text():
+    from ecolex.models import DocumentText
     solr = EcolexSolr()
     doc_number = solr.solr.search('type:legislation').hits
     rows = 100
@@ -20,11 +21,11 @@ def update_legislation_full_text():
         docs = solr.solr.search('type:legislation', start=index, rows=rows)
         new_docs = []
         for doc in docs:
-            if DocumentText.objects.filter(doc_id=doc['id']):
-                continue
-            if 'legLinkToFullText' in doc:
-                print(doc['legLinkToFullText'])
-                file_obj = get_file_from_url(doc['legLinkToFullText'])
+            url = doc.get('legLinkToFullText', None)
+            if url:
+                if DocumentText.objects.filter(url=url):
+                    continue
+                file_obj = get_file_from_url(url)
                 doc_size = file_obj.getbuffer().nbytes
                 doc['text'] = solr.extract(file_obj)
                 new_docs.append(doc)
