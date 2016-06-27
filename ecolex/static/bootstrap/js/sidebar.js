fd51400732cdd67fa5d4e9623ee62df217954314
triplem1 <themacaneata@gmail.com>
2016-06-27 13:15:25 +0300

    filtered results on mobile device issue

--- ecolex/static/bootstrap/js/sidebar.js
+++ ecolex/static/bootstrap/js/sidebar.js
@@ -2,85 +2,85 @@
 
 (function ($) {
 
- $(document).ready(function(){
+    $(document).ready(function () {
 
-    $body = $("body");
-    $filters = $('#filters');
-    $backdrop = $('#backdrop');
-    $filterTrigger = $('#filter-trigger');
-    $results=$('#results');
-    // $last=$filters.lastChild;
-    $filterTrigger.click(function() {
-        console.log('opening sidebar');
-        // event.stopPropagation();
-        $filters.addClass('open');
-        $body.addClass("sidebaropen");
+        $body = $("body");
+        $filters = $('#filters');
+        $backdrop = $('#backdrop');
+        $filterTrigger = $('#filter-trigger');
+        $results = $('#results');
+        // $last=$filters.lastChild;
+        $filterTrigger.click(function () {
+            console.log('opening sidebar');
+            // event.stopPropagation();
+            $filters.addClass('open');
+            $body.addClass("sidebaropen");
+        });
+
+        $backdrop.on('click', function () {
+            console.log('closing sidebar');
+            $filters.removeClass('open');
+            $body.removeClass('sidebaropen');
+        });
+
+
+
+
+
+        if (window.location.href.indexOf("?q=") > -1) {
+            $results.addClass('shown')
+        }
+
+
+
+        /*
+        
+         $body = $("body");
+        $body.click(function() {
+        console.log("sdsd");
+        
+        });
+        
+        
+             function showValues() {
+            var fields = $( ":input" ).serializeArray();
+            // $( "#results" ).empty();
+            jQuery.each( fields, function( i, field ) {
+             
+             if((field.value!=" ")&&(field.value != null)){
+              $("#results").append( "<li>" + field.value + " " + "</li>") ;
+        }
+        
+        
+        // $('results').find('<li>').each(function(){
+        //     if($(this)==" ")
+        //         $(this).remove();
+        // });
+        
+        
+        
+        
+            });
+          }
+         
+          $( ":checkbox, :radio" ).click( showValues );
+          $( "select" ).change( showValues );
+          showValues();
+        */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     });
-    
-    $backdrop.on('click', function() {
-        console.log('closing sidebar');
-        $filters.removeClass('open');
-        $body.removeClass('sidebaropen');
-    });
 
-    
- 
-
-
-if(window.location.href.indexOf("?q=") > -1){
- $results.addClass('shown')
-}
-
-
-
-/*
-
- $body = $("body");
-$body.click(function() {
-console.log("sdsd");
-
-});
-
-
-     function showValues() {
-    var fields = $( ":input" ).serializeArray();
-    // $( "#results" ).empty();
-    jQuery.each( fields, function( i, field ) {
-     
-     if((field.value!=" ")&&(field.value != null)){
-      $("#results").append( "<li>" + field.value + " " + "</li>") ;
-}
-
-
-// $('results').find('<li>').each(function(){
-//     if($(this)==" ")
-//         $(this).remove();
-// });
-
-
-
-
-    });
-  }
- 
-  $( ":checkbox, :radio" ).click( showValues );
-  $( "select" ).change( showValues );
-  showValues();
-*/
-
-
-
-
-
-
-
-
-
-
-
-   
-
-
-});
-
-}(jQuery));
+} (jQuery));
