56f127b34cc5c85d3322fb77375f406515ca97f8
danbaragan <daniel.mihai.baragan@gmail.com>
2017-02-03 16:39:11 +0200

    Missing changes for the mysql component.

--- ecolex/settings.py
+++ ecolex/settings.py
@@ -100,8 +100,11 @@ WSGI_APPLICATION = 'ecolex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'maria',
+        'NAME': os.environ.get('EDW_MYSQL_DATABASE'),
+        'USER': os.environ.get('EDW_MYSQL_USER'),
+        'PASSWORD': os.environ.get('EDW_MYSQL_PASSWORD'),
     }
 }
 
