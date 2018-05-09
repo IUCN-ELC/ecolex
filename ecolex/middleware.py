881d6b1ae64b2e7def6db1ced253cdb71310e788
Diana Boiangiu <boiangiudiana58@gmail.com>
2018-05-09 10:35:35 +0300

    [refs #4858] Fix revproxy sentry client IP address

--- ecolex/middleware.py
+++ ecolex/middleware.py
@@ -1,4 +1,13 @@
 from django.conf import settings
+from raven.contrib.django.raven_compat.models import sentry_exception_handler
+
+
+class SentryForwardedForMiddleware(object):
+    def process_exception(self, request, exception):
+        request.META['REMOTE_ADDR'] = (
+                request.META.get('HTTP_X_FORWARDED_FOR') or
+                request.META['REMOTE_ADDR'])
+        sentry_exception_handler(request=request)
 
 
 class CacheControlMiddleware(object):
